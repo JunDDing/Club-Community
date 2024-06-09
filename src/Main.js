b463bff25c1f4f975cf8fef7ff5de1a267c1c028
usernamesungjun <chltmdwns9@naver.com>
2024-06-10 01:30:49 +0900

    메인, 헤더추가

---
+++ src/Main.js
@@ -0,0 +1,38 @@
+import './Main.css';
+
+function Main() {
+  return (
+    <div className="App">
+       <div class="content">
+            <div class="left">
+                <div class="section">
+                    <h2>동아리 행사 공지</h2>
+                
+                </div>
+            </div>
+            <div class="right">
+                <div class="section">
+                    <h2>부원 모집 게시판</h2>
+            
+                </div>
+            </div>
+        </div>
+
+        <div class="activityPhotoContent">
+            <div class="section">
+                <h2>활동 사진 (최신순)</h2>
+
+            </div>
+        </div>
+
+        <div class="activityVideoContent">
+            <div class="section">
+                <h2>활동 영상</h2>
+
+            </div>
+        </div>
+    </div>
+  );
+}
+
+export default Main;
