311ef064bec8e0e2ed00ddd1c4478aa1bae0bef9
Junhyun <dole614@naver.com>
2024-06-10 12:54:21 +0900

    수정

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import MainPage from "./Main/MainPage";
+import LoginPage from "./Login/LoginPage";
+import RegisterPage from "./Register/RegisterPage";
 
 function App() {
+  const links = [
+    { path: "", element: <MainPage /> },
+    {
+      path: "/login",
+      element: <LoginPage />,
+    },
+    {
+      path: "/register",
+      element: <RegisterPage />,
+    },
+  ];
+
+  const linkPage = () => {
+    return links.map((route) => {
+      return (
+        <Route key={route.path} path={route.path} element={route.element} />
+      );
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>{linkPage()}</Routes>
+    </BrowserRouter>
   );
 }
 
