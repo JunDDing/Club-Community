d65d139dd2f8e67843190ae60a84b882eae7ef39
Junhyun <dole614@naver.com>
2024-06-19 01:46:31 +0900

    경로 추가

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import MainPage from "./Main/MainPage";
 import LoginPage from "./Login/LoginPage";
 import RegisterPage from "./Register/RegisterPage";
 import LoginHandler from "./Login/components/LoginHandler";
+import AdminPage from "./Admin/AdminPage";
+import AdminClubMgmtPage from "./Admin/AdminClubMgmtPage";
+import ClubApplicationPage from "./Club/ClubApplicationPage";
 
 function App() {
   const links = [
@@ -19,6 +22,18 @@ function App() {
     {
       path: "/api/callback",
       element: <LoginHandler />,
+    },
+    {
+      path: "/admin",
+      element: <AdminPage />,
+    },
+    {
+      path: "/admin/application/club/list",
+      element: <AdminClubMgmtPage />,
+    },
+    {
+      path: "/club/application",
+      element: <ClubApplicationPage />,
     },
   ];
 
