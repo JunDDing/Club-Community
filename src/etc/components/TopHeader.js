7ea6bc124543f4124920569e3b447c1367cdb39f
Junhyun <dole614@naver.com>
2024-06-20 04:52:06 +0900

    TopHeader 수정

--- src/etc/components/TopHeader.js
+++ src/etc/components/TopHeader.js
@@ -12,6 +12,10 @@ function TopHeader() {
     navigate("/admin");
   };
 
+  const goMasterClubInfoPage = () => {
+    navigate("/master/club/info");
+  };
+
   const goClubApplicationPage = () => {
     navigate("/club/application");
   };
@@ -19,7 +23,11 @@ function TopHeader() {
   return (
     <div className="TopHeader">
       <header>
-        <button onClick={goAdminPage}>시스템 관리자 메뉴</button>
+        {sessionStorage.getItem("isAdmin") === "true" && (
+          <button onClick={goAdminPage}>시스템 관리자 메뉴</button>
+        )}
+        {/* 마스터 회원(동아리 회장)만 가능 */}
+        <button onClick={goMasterClubInfoPage}>동아리 관리</button>
         <button onClick={goClubApplicationPage}>동아리 신청</button>
         <button onClick={goLoginPage}>로그인</button>
       </header>
