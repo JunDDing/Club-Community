532f9a8a5720274b0bc62c2dbb6eebe89d421ea0
Junhyun <dole614@naver.com>
2024-06-19 17:42:44 +0900

    Login 콘솔 출력 수정

--- src/Login/LoginPage.js
+++ src/Login/LoginPage.js
@@ -31,7 +31,7 @@ function LoginPage() {
       },
     })
       .then((res) => {
-        console.log(res);
+        console.log(res.data);
         alert("로그인 성공");
         navigate("/");
       })
