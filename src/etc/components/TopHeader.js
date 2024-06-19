e179139cf3a8ab0f8fd63a335f25c6eaf3741497
Junhyun <dole614@naver.com>
2024-06-19 17:41:56 +0900

    TopHeader 수정

--- src/etc/components/TopHeader.js
+++ src/etc/components/TopHeader.js
@@ -4,6 +4,10 @@ import "../css/TopHeader.css";
 function TopHeader() {
   const navigate = useNavigate();
 
+  const goLoginPage = () => {
+    navigate("/login");
+  };
+
   const goAdminPage = () => {
     navigate("/admin");
   };
@@ -12,16 +16,12 @@ function TopHeader() {
     navigate("/club/application");
   };
 
-  const goRegisterPage = () => {
-    navigate("/register");
-  };
-
   return (
     <div className="TopHeader">
       <header>
         <button onClick={goAdminPage}>관리자 메뉴</button>
         <button onClick={goClubApplicationPage}>동아리 신청</button>
-        <button onClick={goRegisterPage}>회원가입</button>
+        <button onClick={goLoginPage}>로그인</button>
       </header>
     </div>
   );
